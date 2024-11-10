702bff6a1500a227ea3a2490c95d3adda09b5c79
komeno <tkomeno@student.42tokyo.jp>
2024-11-10 08:31:28 +0000

    Initial Commit

---
+++ includes/PmergeMe.hpp
@@ -0,0 +1,7 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <list>
+#include <iostream>
+
+#endif
---
+++ sources/main.cpp
@@ -0,0 +1,8 @@
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    std::cout << "Hello World!" << std::endl;
+    return (0);
+}
